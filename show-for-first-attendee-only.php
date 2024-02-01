b8dd3b0847f961438eff67004f1aaca6f77544d2
Andras Guseo <andras@theeventscalendar.com>
2024-02-01 12:58:48 +0100

    Update show-for-first-attendee-only.php

--- show-for-first-attendee-only.php
+++ show-for-first-attendee-only.php
@@ -1,15 +1,22 @@
 /**
- * Show Attendee Registration Field for first attendee only.
- * (Hide field for subsequent attendees.)
- * Can handle multiple fields. Works with <select> fields only.
+ * Show Attendee Registration Fields for the first attendee only.
+ * (Remove field for subsequent attendees.)
+ * Can handle multiple fields.
+ *
+ * IMPORTANT!!!
+ * Only works if:
+ * - there is only one type of ticket added to the cart
+ * - or all tickets added to the cart have the same fields
  *
  * Usage: Add the snippet to your functions.php file or with a plugin like Code Snippets.
  *
  * @author: Andras Guseo
- * @version: 1.0.0
-  *
+ * @author: Abz Abdul
+ * @version: 1.1.0
+ *
  * Plugins required: Event Tickets Plus
  * Created: January 30, 2024
+ * Updated: February 1, 2024
  */
 function et_hide_fields() {
 	// Bail if we don't have Event Tickets Plus
@@ -17,7 +24,7 @@ function et_hide_fields() {
 		return;
 	}
 
-	// Generic functions.
+	// Generic functions. Load on single event pages and the attendee registration page.
 	if (
 		(
 			is_single()
@@ -26,7 +33,9 @@ function et_hide_fields() {
 		|| tribe( 'Tribe__Tickets__Attendee_Registration__Template' )->is_on_ar_page()
 	) {
 		?>
-		<script type="application/javascript">
+		<script type="application/javascript" id="et_hide_attendee_fields">
+			const hf_log = false;
+
             function fieldLabels() {
                 /**
                  * ADD THE LABELS HERE
@@ -37,65 +46,93 @@ function et_hide_fields() {
                  */
                 const labels = [
                     'Show for first only',
-                    'Another Label',
-                    'Yet Another Label'
+                    'Email Label',
+                    'Telephone Label',
+                    'URL Label',
+                    'Birth date Label',
+                    'Mydate Label',
+	                'Radio label',
+	                'Checkboxx label',
+	                'Dropit'
                 ];
+
+                // Turn the labels into slugs.
                 return labels.map(label => label.toLowerCase().replaceAll(" ", "-"));
             }
 
             function selectors() {
+                // Use the slugs to create selectors.
                 return fieldLabels().map(label => "input[name*='" + label + "'], select[name*='" + label + "']");
             }
 
             function waitForElement(selector, callback) {
-                console.log("HE: starting... -" + selector);
+                if (hf_log) { console.log("HE: starting... -" + selector); }
                 let element;
                 const intervalID = setInterval(function () {
-                    console.log("HE: waiting...");
+                    if (hf_log) { console.log("HE: waiting..."); }
                     element = document.querySelectorAll(selector);
-                    console.log("HE: length " + element.length);
                     if (element.length > 0) {
+                        // Stop waiting
                         clearInterval(intervalID);
                         callback();
                     }
                 }, 500); // Check every 500 milliseconds
             }
 
-            function hideEm() {
-                // Count how many of the given fields do we have in the form.
-                // We use this count to make sure we show/hide the correct number of fields.
-                let count = 0;
-                selectors().forEach(
-                    function (item, index){
-                        if (document.querySelectorAll(item).length > 0) {
-                            count++;
-                        }
+            function delayHideOnModal() {
+                // Need to wait for DOM to be generated
+                setTimeout(hideOnModal, 200 );
+            }
+
+            function hideOnModal() {
+                // Listen for button clicks and field change on the modal.
+                document.querySelectorAll('.tribe-modal__wrapper--ar .tribe-tickets__tickets-item-quantity-number-input').forEach(
+                    function(item) {
+                        item.addEventListener("change", hideFields);
                     }
                 );
-                console.log("HF: count " + count);
+                document.querySelectorAll('.tribe-modal__wrapper--ar .tribe-tickets__tickets-item-quantity-add').forEach(
+                    function(item) {
+                        item.addEventListener('click', hideFields);
+                    }
+                );
+                document.querySelectorAll('.tribe-modal__wrapper--ar .tribe-tickets__tickets-item-quantity-remove').forEach(
+                    function(item) {
+                        item.addEventListener('click', hideFields);
+                    }
+                );
+                hideFields();
+            }
+
+            function hideFields() {
+                // Radio buttons and checkboxes can have more <input> elements, but we only need to remove
+                // one parent element. We need to discount them accordingly.
+                // Count ALL fields to be hidden on the form, and divide by the number of tickets.
+                // This is how many fields we need to skip for the first attendee.
+                let numAttendees = document.querySelectorAll('.tribe-tickets__attendee-tickets-item').length;
 
                 // Get all the fields.
                 const fields = document.querySelectorAll(selectors().join(', '));
-
+                if (hf_log) { console.log("HF: fields found - " + fields.length); }
                 // Hide the fields for attendees, except the first one.
                 if (fields.length > 0) {
-                    console.log("HF: " + fields[0]);
                     fields.forEach(
                         function (item, index) {
-                            //console.log("HF: " + item + "-" + fieldLabels().length);
                             // Skip the first attendee
-                            if (index < count ) { //fieldLabels().length) {
-                                //console.log("HE: skipping " + index);
+                            if (index < fields.length/numAttendees ) {
+                                if (hf_log) { console.log("HE: skipping " + index); }
                                 return;
                             }
 
-                            // Unset required attribute.
-	                        //console.log("HE: processing " + index);
-	                        item.removeAttribute("required");
-	                        item.removeAttribute("aria-required");
+                            if (hf_log) { console.log('HF: processing item - ' + index); }
 
-                            // Hide the field.
-                            item.parentElement.parentElement.style.display = 'none';
+                            // Unset required attribute.
+                            item.removeAttribute("required");
+                            item.removeAttribute("aria-required");
+
+                            // Remove the field.
+                            if (hf_log) { console.log('HF: closest - ' + item.closest('.tribe-tickets__form-field')); }
+                            item.closest('.tribe-tickets__form-field').remove();
                         }
                     );
                 }
@@ -103,33 +140,23 @@ function et_hide_fields() {
 		</script><?php
 	}
 
-	// Run on Single Event pages.
+	// Listener. Load on Single Event pages.
 	if (
 		is_single()
 		&& get_post_type() == 'tribe_events'
 	) {
 		?>
-		<script type="application/javascript">
-            // Wait for the ticket modal to appear.
-            waitForElement('div.tribe-modal__wrapper--ar', hideOnModal);
-
-            function hideOnModal() {
-                // Listen for button clicks and field change.
-                document.querySelector('.tribe-modal__wrapper--ar .tribe-tickets__tickets-item-quantity-number-input').addEventListener("change", hideEm);
-                document.querySelector('.tribe-modal__wrapper--ar .tribe-tickets__tickets-item-quantity-add').addEventListener("click", hideEm);
-                document.querySelector('.tribe-modal__wrapper--ar .tribe-tickets__tickets-item-quantity-remove').addEventListener("click", hideEm);
-
-                    hideEm();
-            }
+		<script type="application/javascript" id="et_hide_attendee_fields_modal_trigger">
+			document.querySelector('#tribe-tickets__tickets-form #tribe-tickets__tickets-submit').addEventListener('click', delayHideOnModal);
 		</script>
 		<?php
 	}
 
-	// Run on Attendee Registration page
+	// Listener. Load on the Attendee Registration page.
 	if ( tribe( 'Tribe__Tickets__Attendee_Registration__Template' )->is_on_ar_page() ) {
 		?>
-		<script type="application/javascript">
-            waitForElement('.tribe-tickets__form.tribe-tickets__attendee-tickets-item', hideEm);
+		<script type="application/javascript" id="et_hide_attendee_fields_trigger">
+            waitForElement('.tribe-tickets__form.tribe-tickets__attendee-tickets-item', hideFields);
 		</script>
 		<?php
 	}
